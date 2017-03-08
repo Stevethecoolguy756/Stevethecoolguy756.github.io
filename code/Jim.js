76d4f68981eb49582702ab9a77e23733e7a0254a
Stevethecoolguy <Stevethecoolguy756@gmail.com>
2017-03-08 08:22:10 -0500

    Add files via upload

---
+++ code/Jim.js
@@ -0,0 +1,16 @@
+//shirt
+noStroke();
+fill (0, 0, 255);
+rect (205, 155, 95, 200);
+//head
+stroke (75, 75, 75);
+fill (255, 255, 195);
+ellipse (250, 155, 125, 125);
+//eyes
+fill (0, 0, 0);
+ellipse (220, 135, 125, 125);
+fill (0, 0, 0);
+ellipse (275, 135, 10, 10);
+//mouth
+fill (0, 0, 0);
+rect (210, 175, 65, 1);
