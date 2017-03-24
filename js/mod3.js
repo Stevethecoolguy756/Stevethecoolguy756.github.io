cd00c71c50db86f49f8f573de4ecffdee9d94700
Stevethecoolguy <Stevethecoolguy756@gmail.com>
2017-03-24 07:56:45 -0400

    Update mod3.js

--- js/mod3.js
+++ js/mod3.js
@@ -4,7 +4,7 @@
   btn.innerHTML = 'Add Sprite';
   btn.onclick = function(e){
     var newSprite = document.createElement('img');
-    newSprite.src = 'images/guard.png';
+    newSprite.src = 'images/guard copy 2.png';
     document.body.appendChild(newSprite);
   };
   document.body.appendChild(btn);
