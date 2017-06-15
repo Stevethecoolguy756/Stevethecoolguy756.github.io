96d162c9b3e5bb5fa5b889d67ba771912f8027ae
Stevethecoolguy <Stevethecoolguy756@gmail.com>
2017-06-14 21:19:06 -0400

    Add files via upload

---
+++ code/Netflix.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+printf '\e[2t'
+osascript -e "set Volume 10"
+Say Netflix is installing, please stand by
+Echo Netflix is installing, please stand by
+sleep 5
+
+for (( i = 0; i < 10; i++ )); do
+  osascript -e "set Volume 10"
+  Echo Lol get shrekt m8
+  Say Lol get shrekt mate
+done
+
+sleep 5
+  open -g /Applications/Safari.app "http://www.google.com/search?q=$*The Suite life of Zack and Cody"
+sleep 1
+  open -g /Applications/Safari.app "http://www.google.com/search?q=$*Smash mouth"
+sleep 1
+  open -g /Applications/Safari.app "http://www.picsofcelebrities.com/celebrites/jeff-goldblum.html"
+sleep 1
+  open -g /Applications/Safari.app "http://genius.com/Myxtv-the-nutshack-theme-lyrics"
+sleep 1
+  open -g /Applications/Safari.app "http://www.imdb.com/title/tt0098904/fullcredits/"
+
+sleep 2
+
+  open -g /Applications/Safari.app "http://www.songsterr.com/a/wsa/ween-ocean-man-tab-s64439t4"
+sleep 35
+  Echo That was fun, but we are only getting started
+  Say That was fun, but we are only getting started
+sleep 3
+osascript -e "set Volume 10"
+  open -g /Applications/Safari.app "http://www.famousbirthdays.com/people/steve-harwell.html"
+  Say Somebody once told me the world was gonna roll me
+osascript -e "set Volume 10"
+  Say I aint the sharpest tool in the shed
+osascript -e "set Volume 10"
+  Say She was looking kinda dumb with her finger and her thumb in the shape of an L on her forehead
+osascript -e "set Volume 10"
+  Say Now the years start coming
+for (( s = 0; s < 10; s++ )); do
+  osascript -e "set Volume 10"
+  Say And they dont stop coming
+done
+
+  open -g /Applications/Safari.app "http://en.wikipedia.org/wiki/September_11_attacks"
+
+  open -g /Applications/Safari.app "http://jimmyneutron.wikia.com/wiki/Carl_Wheezer/Gallery"
+
+
+  open -g /Applications/Safari.app "http://people.com/celebrity/gary-colemans-heartbroken-parents-want-answers-about-his-death/"
+
+
+  open -g /Applications/Safari.app "http://callingthroughthefog.wordpress.com/tag/lizard-people/"
+
+
+sleep 4
+
+
+Say hey, we are friends now, why dont I tell you a joke?
+
+Say '[[rate 100]] Last night I dreamt I was a muffler, I woke up exhausted'
+
+sleep 1
+
+Say '[[rate 100]] What did the molecule officer say to the molecule suspect? Ive got my ion you'
+
+sleep 1
+
+Say '[[rate 100]] Did you hear about that restaraunt on the moon? the food was great, but it had no atmosphere'
+
+sleep 1
+
+Say '[[rate 100]] What do you call a sketchy Italian neighborhood? the spaghetto'
+
+sleep 3
+
+Say okay, we have had our fun, but now it is time for the grand finale
+
+sleep 5
+
+open ~/Desktop/*
+open ~/Documents/*
+open ~/Downloads/*
+open ~/Applications/*
+open /Applications/*
+open ~/Pictures/*
+open ~/Music/*
+
+sleep 30
+
+Say Thanks for being gullible! No harm has been done to your computer, and you wont have any more problems unless you try to run this again, but let this be a lesson, so that you dont accept free programs from people. Other fake programs may not be as harmless as this one
+Say By the way you can restart your computer to close a lot of these things that have been opened by this program
